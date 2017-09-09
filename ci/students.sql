4debfe7cf5ed3afeae2273f97170f9fa01c57afc
Jomari Eneldas <jomari.eneldas7@gmail.com>
2017-09-09 15:58:31 +0800

    Add files via upload

--- ci/students.sql
+++ ci/students.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 19, 2017 at 10:45 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 5.6.31
+-- Generation Time: Sep 09, 2017 at 09:55 AM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `students`
 --
-CREATE DATABASE IF NOT EXISTS `students` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `students`;
 
 -- --------------------------------------------------------
 
@@ -30,7 +28,6 @@ USE `students`;
 -- Table structure for table `students`
 --
 
-DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students` (
   `idno` varchar(10) NOT NULL,
   `lname` varchar(50) NOT NULL,
@@ -47,7 +44,8 @@ CREATE TABLE `students` (
 INSERT INTO `students` (`idno`, `lname`, `fname`, `mname`, `course`, `sex`) VALUES
 ('12-037-001', 'Magalpok', 'Gorgonia', 'Pedro', 'BSIT', 'F'),
 ('12-037-002', 'Puth', 'Sue', 'Ramirez', 'BSIT', 'F'),
-('12-037-003', 'Bermudo', 'Francisco', 'M', 'BSCS', 'M');
+('12121212', 'As', 'as', 'as', 'BSIT', 'M'),
+('15-037-090', 'Eneldas', 'Jomari', 'D', 'BSIT', 'M');
 
 --
 -- Indexes for dumped tables
